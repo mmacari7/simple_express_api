0d9aa3bb02baea5986adc75c1f1b7108854bbb03
Michael <mmacari7@gmail.com>
2019-03-09 20:06:17 -0700

    Update about.js

--- routes/about.js
+++ routes/about.js
@@ -11,7 +11,7 @@ router.get("/", async (req, res) => {
         const aboutJson = {
             "name": "Michael Macari",
             
-            "cwid": "10432224",
+            "cwid": "Null",
             
             "biography": "My name is Michael Macari. I am a masters student in computer science at Stevens institute of technology. I am also a full time research engineer for the STAR center at Stevens Institute of Technology under Hady Salloum. I am currently working on the development of a portable light weight software defined radio platform integrated with with machine learning for an eager approach to unmanned aerial systems signal classification. Apart from my career in software development, I try to keep busy with an active lifestyle.\n I absolutely love board sports. During the winter time snowboarding is one of my favorites, and in the summer I try to surf whenever I get the opportunity. I try to uphold to a healthy lifestyle through physical fitness and a healthy diet. When I am not snowboarding, surfing or working, you can typically find me outside in the sun reading science fiction novels. One of my favorite books is: Surely you're Joking Mr. Feynman. The book is a hilarious intillectual read through the life of Richard P Feynman, a nobel prize winner with a curious mind who worked on the manhattan project with a hand full of other well known geniuses. Don't take my word for it though, give it a read!",
             
